78a3643e00c0f564711184a84a2640dae0c299f8
Eduardo Cintra <educin15@hotmail.com>
2020-08-05 22:40:52 -0300

    Creating the example

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Formik, Form, Field, ErrorMessage } from 'formik'
+import schema from './schema'
+
 import './App.css';
 
 function App() {
+
+  function onSubmit(values) {
+    console.log(values)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Formik
+        onSubmit={onSubmit} 
+        initialValues={{ email: '', password: ''}}
+        validationSchema={schema}
+        render={({ values, isValid }) => (
+        <React.Fragment>
+          <Form>
+            <div className="form-group">
+              <label>Email</label>
+              <Field  name="email" type="text" className="form-control"/>
+              <ErrorMessage name="email" />
+            </div>
+            <div className="form-group">
+              <label>Password</label>
+              <Field  name="password" type="password" className="form-control"/>
+              <ErrorMessage name="password" />
+            </div>
+
+            <div className="form-group">
+              <button 
+                className="btnSubmit"
+                type="submit"
+                disabled={!isValid}
+                >
+                  Entrar
+              </button>
+            </div>
+        </Form>
+
+          
+        </React.Fragment>
+        )}
+      />
     </div>
   );
 }
